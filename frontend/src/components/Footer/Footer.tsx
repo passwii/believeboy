3680e6015f5809fe20b8d762eae0f376a0d9cbe7
Xeon Wii <jr.way123@gmail.com>
2025-03-05 17:37:57 +0800

    Update Footer.tsx

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -100,7 +100,7 @@ const Footer: React.FC = () => {
           <h3>联系方式</h3>
           <CompanyName>彼励扶电子商务（苏州）有限公司</CompanyName>
           <ul>
-            <li>地址：中国(江苏)自由贸易试验区苏州片区建发微时光商业广场 356</li>
+            <li>地址：江苏省苏州市吴江区鑫蹊商务大厦A 座605室</li>
             <li>联系电话：+86 18666468991</li>
             <li>美国电话：+1 (909) 375-0156</li>
             <li>邮箱：marketing@believeboy.com</li>
