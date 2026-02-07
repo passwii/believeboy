496ee5365bc2e0bc90e33ae0041a8ec3c65f5843
damon <jr.way123@gmail.com>
2026-02-07 22:11:18 +0800

    fix: navbar TypeScript 类型修复和 next-env.d.ts 更新

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -75,7 +75,7 @@ const dropdownVariants = {
     scale: 1,
     transition: {
       duration: 0.3,
-      ease: [0.22, 1, 0.36, 1],
+      ease: [0.22, 1, 0.36, 1] as [number, number, number, number],
       staggerChildren: 0.08,
     },
   },
@@ -92,7 +92,7 @@ const menuItemVariants = {
   visible: {
     opacity: 1,
     x: 0,
-    transition: { duration: 0.4, ease: [0.22, 1, 0.36, 1] },
+    transition: { duration: 0.4, ease: [0.22, 1, 0.36, 1] as [number, number, number, number] },
   },
 };
 
